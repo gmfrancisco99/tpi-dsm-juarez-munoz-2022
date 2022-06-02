f618d34e65f0f3af09a4cffeff68c32030c9a08f
Sofía Juarez <sjuarez@simtlix.com>
2022-06-02 13:56:48 -0300

    AlbumList class changed to functional with async/await

--- components/AlbumList.js
+++ components/AlbumList.js
@@ -1,37 +1,32 @@
-import React, {Component} from 'react';
-import {ScrollView, Text, View} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ScrollView, Text, View } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
-class AlbumList extends Component {
-  state = {photoset: null};
 
-  /* 
-  const getPhotos = async() => {
-    let response;
-    try{
-      response = await axios.get();
-      setPhotos(response.data.photoset.photo);
-    } catch(err) {
-      throw new BadRequestException(err);
+
+const AlbumList = (props) => {
+  const [photoset, setPhotoset] = useState(null);
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      let response;
+      try {
+        response = await axios.get(
+          'https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=6e8a597cb502b7b95dbd46a46e25db8d&user_id=137290658%40N08&format=json&nojsoncallback=1'
+        );
+        setPhotoset(response.data.photosets.photoset)
+      } catch (err) {
+        console.log(err);
+      }
     }
-  }
-  getPhotos();
-  */
-  componentWillMount() {
-    axios
-      .get(
-        'https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=6e8a597cb502b7b95dbd46a46e25db8d&user_id=137290658%40N08&format=json&nojsoncallback=1',
-      )
-      .then((response) =>
-        this.setState({photoset: response.data.photosets.photoset}),
-      );
-  }
+    getPhotos();
+  }, []);
 
-  renderAlbums() {
-    return this.state.photoset.map((album) => (
+  function renderAlbums() {
+    return photoset.map((album) => (
       <AlbumDetail
-        navigation={this.props.navigation}
+        navigation={props.navigation}
         key={album.id}
         title={album.title._content}
         albumId={album.id}
@@ -39,19 +34,15 @@ class AlbumList extends Component {
     ));
   }
 
-  render() {
-    console.log(this.state);
-
-    if (!this.state.photoset) {
-      return <Text>Loading...</Text>;
-    }
-
-    return (
-      <View style={{flex: 1}}>
-        <ScrollView>{this.renderAlbums()}</ScrollView>
+  return (
+    (!photoset ?
+      <Text>Loading...</Text>
+      :
+      <View style={{ flex: 1 }}>
+        <ScrollView>{renderAlbums()}</ScrollView>
       </View>
-    );
-  }
+    )
+  )
 }
 
 export default AlbumList;
